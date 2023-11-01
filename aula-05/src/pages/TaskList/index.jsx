c8edd8a5f32afd1e6696b5263f81fb7addef6111
leignel <cristian.leignel@gmail.com>
2023-11-01 20:28:04 -0300

    Atualizações Aula 05

--- aula-05/src/pages/TaskList/index.jsx
+++ aula-05/src/pages/TaskList/index.jsx
@@ -1,8 +1,12 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+const url = "https://6542c27001b5e279de1f8a8c.mockapi.io/tasklist";
 
 const TaskList = () => {
   const [novaTarefa, setNovaTarefa] = useState("");
   const [tarefas, setTarefas] = useState([]);
+  const [loading, setLoading] = useState(false);
 
   const cadastrar = () => {
     setTarefas([...tarefas, novaTarefa]);
@@ -10,12 +14,38 @@ const TaskList = () => {
     setNovaTarefa("");
   };
 
-  const excluirTarefa = (ind) => {
-    console.log("Excluir Tarefa: ", ind);
-    const novoArray = tarefas.filter((item, index) => index != ind);
+  const excluirTarefa = (id) => {
+    console.log("Excluir Tarefa: ", id);
+    const novoArray = tarefas.filter((item) => item.id != id);
     console.log(novoArray);
     setTarefas(novoArray);
   };
+
+  // async function getTasks(){}
+  const getTasks = async () => {
+    setLoading(true);
+    try {
+      // const dados = await fetch(
+      //   "https://6542c27001b5e279de1f8a8c.mockapi.io/tasklist"
+      // );
+      // const infos = await dados.json();
+      const { data } = await axios.get(url);
+      console.log(data);
+      setTarefas(data);
+    } catch (err) {
+      console.log(err);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  // O useEffect é executado logo após a montagem do componente na tela
+  // UseEffect( () => {}, [])
+  useEffect(() => {
+    getTasks();
+  }, []);
+
+  // if (loading) return <h1>GARREGANDO...</h1>;
 
   return (
     <main>
@@ -31,10 +61,10 @@ const TaskList = () => {
       </section>
       <section>
         <h2>Lista de Tarefas</h2>
-        {tarefas.map((item, index) => (
-          <div key={index}>
-            <h3>{item}</h3>
-            <button onClick={() => excluirTarefa(index)}>Excluir</button>
+        {tarefas.map((item) => (
+          <div key={item.id}>
+            <h3>{item.title}</h3>
+            <button onClick={() => excluirTarefa(item.id)}>Excluir</button>
           </div>
         ))}
       </section>
